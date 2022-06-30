dfb9936a77bb63c2d45e843308dc5fc80a42c7d2
Sudhir <r.sudhirmandal@gmail.com>
2022-06-30 18:39:05 +0530

    SpringSecurityMVC

---
+++ SpringMVCSecurity/src/main/java/com/sudhir/springmvcsecurity/security/config/WebSecurityApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.sudhir.springmvcsecurity.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
